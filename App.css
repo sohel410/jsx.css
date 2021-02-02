9142f2283eb52fafde40e721d7dfc14564cfd935
sohel410 <62168162+sohel410@users.noreply.github.com>
2021-02-02 21:49:17 +0530

    Create App.css

---
+++ App.css
@@ -0,0 +1,26 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div> 
+    <header  className = "dodo">
+     THIS IS RED FONT
+    </header> <br></br> <br></br>
+    </div>
+    <div>
+    <header  className = "dd">
+     THIS IS BLUE FONT
+    </header>
+    </div> <br></br> <br></br>
+    <div>
+    <header  className = "gg">
+     THIS IS GREEN FONT
+    </header>
+    </div>
+       </div>
+  );
+}
+
+export default App;
